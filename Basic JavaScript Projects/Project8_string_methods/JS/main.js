28fe33bd10c9c55c18e547ea08717b3b10cad48f
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-10 11:05:40 -0800

    asignacion hasta el paso 168

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -6,3 +6,21 @@ function full_sentence() {
     var whole_sentence = part1.concat(part2, part3);
     document.getElementById("concat").innerHTML = whole_sentence;
 }
+// metodo slice
+function slice_method() {
+    var sentence = "All work and no play makes Jhony a dull boy";
+    var section = sentence.slice(27, 33);
+    document.getElementById("slice").innerHTML = section;
+}
+
+// metodo string
+function string_method() {
+    var X =182;
+    document.getElementById("numbers_to_string").innerHTML = X.toString();
+}
+
+//metodo precision
+function precision_method() {
+    var X = 12938.30129;
+    document.getElementById("precision").innerHTML = X.toPrecision(10);
+}
