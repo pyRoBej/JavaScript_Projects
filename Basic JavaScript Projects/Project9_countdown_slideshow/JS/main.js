beb789731ea7d07d48979fa5ad3b549b39b0f1f3
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-10 13:00:14 -0800

    asignacion hasta el paso 174 (presentacion de diapositivas)

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,35 @@
+//metodo countdown
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000); 
+        if (seconds == -1) {
+            alert("time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+//pagina con slideshow (presentacion de diapositivas)
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
