b3f5d64af5bd72aa5f8df39761350f4213e471f1
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-15 00:41:13 -0800

    Proyecto pizza terminado

--- Pizza_Project/pizzaJavaScript/pizza.js
+++ Pizza_Project/pizzaJavaScript/pizza.js
@@ -1,126 +1,58 @@
-// almacena un objeto para realizar un seguimiento de los valores
-const Calculator = {
-    // esto se muestra 0 en la pantalla
-    Display_Value: '0',
-    // esto contendrá la primera operación y para cualquier expresión, lo establecemos en nulo para
-    First_Operand: null,
-    Wait_Second_Operand: false,
-    // esto mantendrá el operador, lo configuramos como nulo por ahora
-    operator: null,
+function getReceipt(){
+    // esto inicializa nuestra cadena para que pueda pasar de
+// función para funcionar creciendo línea por línea en un recibo completo
+    var text1 = "<h3> You Ordered:</h3>";
+    var runningTotal = 0;
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked){
+            var selectedSize = sizeArray[i].value;
+            text1 = text1+ selectedSize+"<br>";
+        }
+    }
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6;
+    } else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+    runningTotal = sizeTotal;
+    console.log(selectedSize+" = $"+ sizeTotal+".00");
+    console.log("size text1: " + text1);    
+    console.log("subtotal: $"+runningTotal+ ".00");
+    // estas variables se pasarán a cada función
+    getTopping(runningTotal, text1);
     };
-
-    // esto modifica los valores cada vez que se hace clic en un botón
-    function Input_Digit(digit){
-        const { Display_Value, Wait_Second_Operand } = Calculator;
-        // estamos comprobando si esperar el segundo operando es verdadero y establecemos
-        // muestra el valor de la clave en la que se hizo clic
-        if(Wait_Second_Operand === true){
-            Calculator.Display_Value = digit;
-            Calculator.Wait_Second_Operand = false;
-
+    
+    function getTopping(runningTotal, text1){
+        var toppingTotal = 0;
+        var selectedTopping = [];
+        var toppingArray = document.getElementsByClassName("toppings");
+        for (var j = 0; j< toppingArray.length;j++){
+            if ( toppingArray[j].checked){
+                selectedTopping.push(toppingArray[j].value);
+                console.log("selected topping item: ("+toppingArray[j].value+")");
+                text1 = text1+toppingArray[j].value+"<br>";
+            }
+        }
+        var toppingCount = selectedTopping.length;
+        if (toppingCount > 1 ) {
+            toppingTotal = (toppingCount - 1);
         } else {
-            // esto sobrescribe el valor de visualización si el valor actual es 0
-            // de lo contrario, se agrega
-            Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+            toppingTotal = 0;
         }
-    }
-    // esta sección maneja puntos decimales
-    function Input_Decimal(dot) {
-        // esto asegura que el clic accidental del punto decimal
-        // no causa errores en su operación
-        if (Calculator.Wait_Second_Operand === true) return;
-        if(!Calculator.Display_Value.includes(dot)) {
-            // estamos diciendo que si el valor de visualización no contiene un punto decimal
-            // queremos agregar un punto decimal
-            Calculator.Display_Value += dot;
-        }
-    }
-
-     // esta sección maneja operadores
-    function Handle_Operator(Next_Operator){
-        const { First_Operand, Display_Value, operator} = Calculator
-        // cuando se presiona una tecla de operador, convertimos el número actual
-        // se muestra en la pantalla a un número y luego almacena el resultado en
-        // calculator.firtoperand si aún no existe
-        const Value_of_Input = parseFloat(Display_Value);
-        // verifica si ya existe un operador y si esperar segundo operando es verdadero
-        // luego actualiza el operador y existe desde la función
-        if (operator && Calculator.Wait_Second_Operand) {
-            Calculator.operator = Next_Operator;
-            return;
-        }
-        if (First_Operand == null) {
-            Calculator.First_Operand = Value_of_Input;
-        }else if (operator) {
-            // comprueba si ya existe un operador
-            const Value_Now = First_Operand || 0 ;
-            // si el operador existe, la búsqueda de propiedades se realiza para el operador
-            // en el objeto de realizar el cálculo y la función que coincide con el
-            // se ejecuta el operador
-            let result = Perform_Calculation[operator](Value_Now, Value_of_Input);
-            // aquí agregamos una cantidad fija de número después del decimal
-            result = Number(result).toFixed(9)
-            // esto eliminará los ceros finales
-            result = (result * 1).toString()
-            Calculator.Display_Value = parseFloat(result);
-            Calculator.First_Operand = parseFloat(result);
-            
-        }
-        Calculator.Wait_Second_Operand = true;
-        Calculator.operator = Next_Operator;
-
-    }
-
-const Perform_Calculation = {
-    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
-    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
-    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
-    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
-    '=': (First_Operand, Second_Operand) => Second_Operand
-};
-
-function Calculator_Reset() {
-    Calculator.Display_Value = '0';
-    Calculator.First_Operand = null;
-    Calculator.Wait_Second_Operand = false;
-    Calculator.operator = null;
-}
-
-// esta funcion actualiza la pantalla con el contenido de Display_Value
-function Update_Display() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = Calculator.Display_Value;
-}
-
-Update_Display();
-//esta seccion supervisa los clics de los botones
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-    // la variable de destino respresenta un objeto al que se le hizo click
-    const { target } = event;
-    // si al elemento al que se le hizo click no es un boton, salga de la funcion
-    if (!target.matches('button')) {
-        return;
-    }
-
-    if (target.classList.contains('operator')) {
-        Handle_Operator(target.value);
-        Update_Display();
-        return;
-    }
-
-    if (target.classList.contains('decimal')) {
-        Input_Decimal(target.value);
-        Update_Display();
-        return;
-    }
-    // asegura que AC borre los numeros de la calculadora
-    if (target.classList.contains('all-clear')) {
-        Calculator_Reset(target.value);
-        Update_Display();
-        return;
-    }
-
-    Input_Digit(target.value);
-    Update_Display();
-})
+        runningTotal = (runningTotal + toppingTotal);
+        console.log("total selected topping items: "+toppingCount);
+        console.log(toppingCount+ "topping - 1 free topping = "+"$"+toppingTotal+".00");
+        console.log("topping text1: "+text1);
+        console.log("purchase total: "+"$"+runningTotal+".00");
+        document.getElementById("showText").innerHTML=text1;
+        document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
+        runningTotal+".00"+"</strong></h3>";
+    }; 
